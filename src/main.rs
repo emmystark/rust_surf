11d68608cbb9d1eac0c31734b498dee81eeb1727
emmystark <nkereuwememmanuel03@gmail.com>
2025-07-01 21:33:11 +0100

    uses_of_match

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("\nLet's calculate the sum of two numbers:");
     calculate_sum();
     constants_();
+    match_();
 }
 
 fn calculate_sum() {
@@ -21,8 +22,50 @@ fn calculate_sum() {
     println!("The sum of {} and {} is {}", x, y, sum);
 }
 
+/// This function prints out the maximum number of points allowed
+/// and the result of the && and || operators
 fn constants_() {
     // Constants are immutable and must have a type annotation
     const MAX_POINTS: u32 = 100_000;
     println!("The maximum points are {}", MAX_POINTS);
+
+    // Using of && and || operators
+    let a = true;
+    let b = false;
+    let c = a && b; // Logical AND
+    let d = a || b; // Logical OR
+    println!("a && b = {}, a || b = {}", c, d);
+
+    let age = 15;
+    let can_vote = age >= 18;
+
+    println!("Can vote? {}", can_vote);
+
+    if 120 < 102 {
+        println!("90 is less than 102");
+    } else if 100 < 102 {
+        println!("100 is less than 102");
+    } else {
+        println!("90 is greater than 102");
+    }
+}
+
+fn match_() {
+    let day = 60;
+    // match day {
+    //     1 => println!("Monday"),
+    //     2 => println!("Tuesday"),
+    //     3 => println!("Wednesday"),
+    //     4 => println!("Thursday"),
+    //     5 => println!("Friday"),
+    //     6 => println!("Saturday"),
+    //     7 => println!("Sunday"),
+    //     _ => println!("Invalid day."),
+    // }
+
+    match day {
+        1 | 2 | 3 | 4 | 5  =>  println!("Weekday!"),
+        6 | 7 => println!("Weekend!"),
+        _ => println!("Invalid day")
+    }
 }
